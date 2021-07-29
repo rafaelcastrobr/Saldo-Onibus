afc915cb9abb709de47ee327c19b54b17541a831
rafaelcastrobr <rafaelcastrobr@gmail.com>
2021-07-29 16:37:02 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,99 @@
 const saldoTotal = document.querySelector('.c-saldo__result');
-let total;
+let histResult = document.querySelector('.c-historico__result--none')
 
+let total;
 let retVal;
 let saveB;
-
+let infoSaldo;
+let recarA = [];
+let diaA = [];
 
 /*
 LocalStorage
 */
 
+// valor e info
 function gravar(num) {
   localStorage.setItem('valor', num);
+  let divisao = (num / 4.40).toFixed(0);
+  let divisaoInt = (num/7.65).toFixed(0);
+
+
+  if (num < 4.39) {
+    infoSaldo = `<p>Saldo insuficiente!<p>`;
+  }else if(num <= 7.64){
+    infoSaldo = `<p><span style="color:red">1</span> passagem de ônibus!</p>`;
+  }else if(num <= 8.79) {
+    infoSaldo = `<p><span style="color:red">1</span> passagem de ônibus
+    ou <span style="color:red">1</span> de Integração!</p>`;
+  }else if(num <= 15.29) {
+    infoSaldo = `<p><span style="color:red">${divisao}</span> passagens de ônibus
+    ou <span style="color:red">1</span> de Integração!</p>`;
+  }else if (num > 8.80){
+    infoSaldo = `<p><span style="color:red">${divisao}</span> passagens de ônibus 
+    ou <span style="color:red">${divisaoInt}</span> de Integrações!</p>`;
+  }
+
+  localStorage.setItem('info', infoSaldo);
+
+  if(localStorage.info){
+    info.innerHTML = infoSaldo;
+  } else {
+    info.innerHTML = `<p>Saldo insuficiente!<p>`
+  }
 }
 
-function gr() {
+const info = document.querySelector('.c-historico__info');
+(function gr() {
   if(localStorage.valor) {
   saveB = localStorage.valor;
   saldoTotal.innerHTML = Number(saveB).toFixed(2);
   total = Number(saveB);
   } else {
     total = 0.00;
-    saldoTotal.innerHTML = total;
+    saldoTotal.innerHTML = total.toFixed(2);
+  }
+
+  if(localStorage.info) {
+    infoSaldo = localStorage.info;
+    info.innerHTML = infoSaldo;
+  } else{
+    info.innerHTML = `<p>Saldo insuficiente!</p>`;
+  }
+
+})();
+
+// recarga
+function gravarRA(num){
+  let data = new Date();
+  let dia = addZero(data.getDate());
+  let mes = addZero(data.getMonth());
+  let ano = addZero(data.getFullYear());
+
+  function addZero(num) {
+    return num >= 10 ? num : `0${num}`;
+  }
+
+  let week = `${dia}-${mes}-${ano}`
+
+  let rec = `R$  ${num.toFixed(2)} no dia <span style="color:red;">${week}</span>`;
+
+  recarA.push(rec);
+  localStorage.setItem('recarga', JSON.stringify(recarA));
+}
+
+(function rs() {
+  if(localStorage.recarga) {
+    recarA = JSON.parse(localStorage.recarga);
+    histResult.innerHTML += `Recargas`
+    for(pos in recarA) {
+        histResult.innerHTML += `<p class="yes">${recarA[pos]}</p>`;
+    }
+  } else {
+    recarA = [];
   }
   
-}
-gr()
-
+})();
 
 /*
 USOU BILHETE
@@ -34,7 +102,6 @@ USOU BILHETE
 // botao ônibus ou metrô
 const oOM = document.querySelector('.c-usar__botao__um');
 oOM.addEventListener('click', () => {
-  const saldoTotal = document.querySelector('.c-saldo__result');
 
   if (total >= 4.39) {
     let valor = 4.40;
@@ -52,7 +119,6 @@ oOM.addEventListener('click', () => {
 // botao ônibus + metrô
 const oOEM = document.querySelector('.c-usar__botao__dois');
 oOEM.addEventListener('click', () => {
-  const saldoTotal = document.querySelector('.c-saldo__result');
 
   if (total >= 7.64) {
     let valor = 7.65;
@@ -69,11 +135,11 @@ oOEM.addEventListener('click', () => {
 });
 
 // botao voltar
+const voltar = document.querySelector('.c-usar__botao__tres')
 const volt = document.querySelector('.c-usar__botao__tres');
 volt.addEventListener('click', () => {
   saldoTotal.innerHTML = retVal.toFixed(2);
   total = retVal;
-  const voltar = document.querySelector('.c-usar__botao__tres')
   voltar.setAttribute('disabled', '');
   gravar(retVal);
 });
@@ -83,9 +149,9 @@ RECARGA
 */
 
 // Aparecer botão
+const recarClic = document.querySelector('#recarregar');
 const recar = document.querySelector('.c-carregar__botao__r');
 recar.addEventListener('click', () => {
-  const recarClic = document.querySelector('#recarregar');
 
   if (document.querySelector('.c-carregar__valor--none')) {
     recarClic.classList.remove('c-carregar__valor--none');
@@ -114,13 +180,13 @@ okClic.addEventListener('click', () =>{
     return;
   }
   if(total !== Number) {
-    const recarClic = document.querySelector('#recarregar');
     const audio = document.querySelector('audio');
     audio.play();
+    audio.volume = 0.1;
     let valorAdd = Number(addRecar.value);
     total += valorAdd;
     let valor = total;
-    let tstotal = total.toFixed(2);
+    let tstotal = total.toFixed(2)
     saldoTotal.innerHTML = tstotal;
     recarClic.classList.remove('c-carregar__valor');
     recarClic.classList.add('c-carregar__valor--none');
@@ -128,6 +194,36 @@ okClic.addEventListener('click', () =>{
     document.querySelector('.c-usar__botao__tres').removeAttribute('disabled');
     addRecar.value = ``;
     gravar(tstotal);
+
+    //historico
+    gravarRA(valorAdd);
+    histResult.innerHTML = ``;
+    histResult.innerHTML += `Recargas`
+    for(pos in recarA) {
+        histResult.innerHTML += `<p class="yes">${recarA[pos]}</p>`;
+    }
+
+  }
+});
+
+/*
+HISTORICO
+*/
+
+const hist = document.querySelector('.c-historico__botao');
+const idHist = document.querySelector('#historico');
+hist.addEventListener('click', () => {
+
+  if(document.querySelector('.yes')) {
+    if (document.querySelector('.c-historico__result--none')) {
+      idHist.classList.remove('c-historico__result--none');
+      idHist.classList.add('c-historico__result');
+    } else {
+      idHist.classList.remove('c-historico__result')
+      idHist.classList.add('c-historico__result--none');
+    }
+  } else {
+    alert('Você não possui recargas para exibir!')
   }
 });
 
@@ -139,12 +235,29 @@ const zerar = document.querySelector('.c-zerar__botao');
 zerar.addEventListener('click', () => {
   if(confirm('Quer mesmo apagar seu saldo?')) {
     total = Number(0.00);
-    saldoTotal.innerHTML = total;
+    saldoTotal.innerHTML = total.toFixed(2);
     gravar(total);
-  } 
+    localStorage.removeItem('valor');
+    voltar.setAttribute('disabled', '');
+
+
+    //historico
+    histResult.innerHTML = ``;
+    localStorage.removeItem('recarga');
+    recarA = [];
+
+    info.innerHTML = `<p>Saldo insuficiente!</p>`
+    localStorage.removeItem('info');
+  }
+  
+
+  if(document.querySelector('.c-carregar__valor')) {
+    recarClic.classList.remove('c-carregar__valor');
+    recarClic.classList.add('c-carregar__valor--none');
+  }
+
+  if (document.querySelector('.c-historico__result')) {
+    idHist.classList.remove('c-historico__result')
+    idHist.classList.add('c-historico__result--none');
+  }
 })
-
-
-
-
-
